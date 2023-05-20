ae16f62c68b1b6db4a8594acf9c75f44f05aff30
AnchalJainn <33244752+AnchalJainn@users.noreply.github.com>
2023-05-20 15:44:08 +0530

    Update runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -1,6 +1,11 @@
+#!/bin/sh -xe
+
 sudo docker container ls --format="{{.ID}}\t{{.Ports}}" | awk "{print $1}"
 if [ ! -z "$container" ]
 then
+echo ${container}
 sudo docker rm -f ${container}
 fi
+
+echo "running docker container on Port 8083"
 sudo docker container run -d -p 8083:80 anchaljaindevops/applebite:latest
