fd745026c6a6cf4be2d311041b8312c03ab01ce3
AnchalJainn <33244752+AnchalJainn@users.noreply.github.com>
2023-05-20 15:37:22 +0530

    Create runDocker.sh

---
+++ runDocker.sh
@@ -0,0 +1,6 @@
+sudo docker container ls --format="{{.ID}}\t{{.Ports}}" | awk "{print $1}"
+if [ -z "$container" ]
+then
+sudo docker rm -f ${container}
+fi
+sudo docker container run -d -p 8083:80 anchaljaindevops/applebite:latest
